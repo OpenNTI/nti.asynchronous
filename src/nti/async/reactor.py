494e34e279bdcfa723d47b938be07c0a6d41dbe6
Josh Zuech <josh.zuech@nextthought.com>
2014-08-29 19:50:49 +0000

    Make sure we load library up front
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.async/trunk@48408 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/async/reactor.py
+++ src/nti/async/reactor.py
@@ -22,6 +22,8 @@ from nti.zodb.interfaces import UnableToAcquireCommitLock
 
 from nti.dataserver.interfaces import IDataserverTransactionRunner
 
+from nti.contentlibrary.interfaces import IContentPackageLibrary
+
 from nti.utils.property import Lazy
 
 from .interfaces import IQueue
@@ -45,6 +47,13 @@ class AsyncReactor(object):
 		self.fail_queue_name = fail_queue
 		self.poll_interval = poll_interval
 		self.queue_interface = queue_interface
+
+		transaction_runner = component.getUtility(IDataserverTransactionRunner)
+		transaction_runner(self._load_library)
+
+	def _load_library(self):
+		library = component.queryUtility(IContentPackageLibrary)
+		getattr(library, 'contentPackages', None)
 
 	@Lazy
 	def queues(self):
